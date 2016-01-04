1a68da254016cc1cd6a30570a76287a4a0f18842
AnithaPal <anitha.aravindvel@gmail.com>
2016-01-04 05:37:48 -0500

    updated body-parser

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,43 +1,15 @@
 
-eventsApp.factory('eventData', function(){
+eventsApp.factory('eventData', function($http, $log){
 	return{
-		event: {
-			name: "Angular Boot Camp",
-			date: 1359781015626,
-			time: "10:30 am",
-			location: {
-			address: "Google Headquarters",
-			city: "Mountain View",
-			province: 'CA'
-			},
-			imageUrl: '/img/angularjs-logo.png',
-			sessions: [
-				{
-					name: 'Directive Masterclass',
-					creatorName: 'Bob Smith',
-					duration: 1,
-					level: 'Advanced',
-						abstract: 'In this lession you ll learn ins and outs of  directives',
-				upVoteCount: 0
-				},
-				{
-					name: 'Scopes for fun and profit',
-					creatorName: 'John Doe',
-					duration: 2,
-					level: 'Introductory',
-					abstract: 'This session we ll take closer look at scopes. Learn what do they do, how do they do',
-					upVoteCount: 0
-				},
-				{
-					name: 'Well Behaved Controllers',
-					creatorName: 'Jane Doe',
-					duration: 4,
-					level: 'Intermediate',
-					abstract: 'Controllers are beginning of everything in Angular',
-					upVoteCount: 0
-				}
-			]
-		}
-		
+		getEvent: function(successcb){
+			$http({method: 'GET', url: '/data/event/1'}).
+				success(function (data, status, headers, config){
+					successcb(data);
+				
+			}).
+ 				error(function(data, status, headers, config){
+				$log.warn(data, status, headers(), config);
+			});	
+		}	
 	};
 })
